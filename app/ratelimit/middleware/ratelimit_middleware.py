c3db9d7e6c93bdce70aa7daa3614ca3307c40cc6
abhayrajsingh099 <abhayrajsingh2091@gmail.com>
2026-02-01 10:47:43 +0530

    Lua script implemented

--- app/ratelimit/middleware/ratelimit_middleware.py
+++ app/ratelimit/middleware/ratelimit_middleware.py
@@ -1,3 +1,5 @@
+import random
+
 from django.http import JsonResponse
 from ratelimit.userlimit import validate_user_limit
 
@@ -8,16 +10,19 @@ class RateLimitMiddleware:
     def __call__(self, request):
         user_ip = request.META.get('REMOTE_ADDR')
 
-        user_limit_info = validate_user_limit(user_ip)
+        try:
+            user_limit_info = validate_user_limit(user_ip)
+        except Exception as e:
+            response = JsonResponse({'error':'Server error'}, status=502)
+            response['Retry-After'] = random.randint(30, 60)
+            return response
 
-        if not user_limit_info:
-            response = JsonResponse({'error':'limit reached try after few mintues'}, status=429)
-            response['Retry-After'] = 60
+        if not user_limit_info['allow']:
+            response = JsonResponse({'error':'limit reached try after few minutes'}, status=429)
+            response['Retry-After'] = user_limit_info['Retry-After']
             return response
 
 
         response = self.get_response(request)
-        # Code to be executed for each request/response after
-        # the view is called.
 
         return response
--- app/ratelimit/userlimit.py
+++ app/ratelimit/userlimit.py
@@ -3,37 +3,61 @@
 """
 import time
 import uuid
+from math import ceil
 from core.redis import redis_client
 
-WINDOW = 60
 r = redis_client # call 'r' for every cmd to interact with redis
-LIMIT = 10
 
-def validate_user_limit(user_ip) -> bool:
-    response = False
+lua_script = """
+local info = {0, 0}
+local unique_key = KEYS[1]
+local limit = tonumber(ARGV[1])
+local window_ms = tonumber(ARGV[2])
+local NOW_MS = tonumber(ARGV[3])
+local req_id = ARGV[4]
+local old_timestamp = NOW_MS - window_ms
 
+redis.call("ZREMRANGEBYSCORE", unique_key, "-inf", old_timestamp)
+
+local count = redis.call("ZCARD", unique_key)
+
+if count < limit then
+    local message = "req:" .. req_id
+    redis.call("ZADD", unique_key, NOW_MS, message)
+    info[1] = 1
+else
+    local oldest = redis.call("ZRANGE", unique_key, 0, 0, "WITHSCORES")
+    local oldest_score = tonumber(oldest[2])
+    local retry_after_ms = (oldest_score + window_ms) - NOW_MS
+    local retry_after_sec = math.ceil(retry_after_ms/1000)
+    info[2] = math.max(0, retry_after_sec)
+end
+redis.call("EXPIRE", unique_key, math.floor((window_ms * 2) / 1000))
+
+return info
+"""
+WINDOW_SEC = 60*60
+LIMIT = 60
+def validate_user_limit(user_ip) -> dict:
+    info = {'allow':True, 'Retry-After':0}
     # Build Redis key
     unique_key = f"rate_limit:ip:{user_ip}"
+    req_id = str(uuid.uuid4())
 
-    # Get current timestamp
-    NOW = int(time.time()*1000) #milliseconds
+    NOW_MS = int(time.time()*1000) #milliseconds
 
-    # Add timestamp to ZSET
-    r.zadd(unique_key, {f"req:{uuid.uuid4()}":NOW})
+    my_script = r.register_script(lua_script)
+    result = my_script(
+        keys=[unique_key],
+        args=[LIMIT, WINDOW_SEC*1000, NOW_MS, req_id]
+    )
 
-    # Remove old timestamps
-    old_timestamp = NOW - WINDOW
-    r.zremrangebyscore(unique_key, "-inf", old_timestamp)
+    if result[0] == 0:
+        info['allow'] = False
+        info['Retry-After'] = result[1]
 
-    count = r.zcard(unique_key)
-    
 
-    if count <= LIMIT:
-        response = True
-
-    r.expire(unique_key, WINDOW*2)
-
-    return response
+    return info
 
 
 
